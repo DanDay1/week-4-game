d5c752124aec712f70c7edadd41b088d938a7bc1
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-07-06 21:52:33 -0400

    got rid of the comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 var wins = 0;
 var losses = 0;
-//var score = 0;
 
 var redValue;
 var blueValue;
@@ -10,19 +9,19 @@ var greenValue;
 var loadTarget;
 var loadCrystal;
 
-var total // = 0;
+var total 
 
 var options;
 
 function targetNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; 
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 var restart = function() {
 
-	loadTarget = targetNumber(19, 120);
+    loadTarget = targetNumber(19, 120);
 
     console.log("found randomNumber is " + loadTarget);
 
@@ -42,7 +41,7 @@ var restart = function() {
 
     greenValue = loadCrystal;
 
-	console.log("red value is " + redValue);
+    console.log("red value is " + redValue);
     console.log("blue value is " + blueValue);
     console.log("yellow value is " + yellowValue);
     console.log("green value is " + greenValue);
@@ -79,56 +78,9 @@ var scoreBoard = function() {
 
 document.onkeyup = function(event) {
 
-    // function targetNumber (l, h) {
-    // 	for (var i = 0; i < 500; i++){
-    // 		function getRandomArbitrary(19, 120) {
-    //   return Math.random() * (19 - 120) + 19;
-    // }
-    // var randomNumber =;
-    // console.log("1 randomNumber is " + randomNumber);
-    // // var randomNumber = options[Math.floor(Math.random() * options.length)];
-    // if(randomNumber >= l && randomNumber <= h) {
-
-    // 	console.log("found randomNumber is " + randomNumber);
-
-    // 	return randomNumber;
-    // }
-    // 	}
-    // }
-    // loadTarget = targetNumber(19, 120);
-
-    // console.log("found randomNumber is " + loadTarget);
-
-    // loadCrystal = targetNumber(1, 12);
-
-    // redValue = loadCrystal;
-
-    // loadCrystal = targetNumber(1, 12);
-
-    // blueValue = loadCrystal;
-
-    // loadCrystal = targetNumber(1, 12);
-
-    // yellowValue = loadCrystal;
-
-    // loadCrystal = targetNumber(1, 12);
-
-    // greenValue = loadCrystal;
-
     restart();
 
     scoreBoard();
-
-    // console.log("red value is " + redValue);
-    // console.log("blue value is " + blueValue);
-    // console.log("yellow value is " + yellowValue);
-    // console.log("green value is " + greenValue);
-
-
-
-    // var blue = Math.floor((Math.random()* 12) + 1);
-    // var yellow = Math.floor((Math.random()* 12) + 1);
-    // var green = Math.floor((Math.random()* 12) + 1);
 
     $(document).ready(function() {
         $('#red').click(function() {
@@ -154,9 +106,5 @@ document.onkeyup = function(event) {
 
     });
 
-    //var score = total;
-
 }
 
-// $('.crystal').on('click', function(){
-// 	score = + parseInt($(this).data('num'));
